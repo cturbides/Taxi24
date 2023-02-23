967dc96e9472a6aff0c464391ac844bd8458a388
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-02-23 15:24:37 +0000

    feat: Pasajero router added to index.js

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const morgan = require('morgan');
 const errorHandler = require('./src/middlewares/errorHandler');
 const invalidPathHandler = require('./src/middlewares/invalidPathHandler');
 
+const pasajeroRouter = require('./src/routers/Pasajero');
+
 const PORT = 3000;
 const app = express();
 
 app.use(express.json());
 app.use(morgan('combined'));
+
+app.use('/api/pasajeros', pasajeroRouter);
 
 app.use(errorHandler);
 app.use(invalidPathHandler);
