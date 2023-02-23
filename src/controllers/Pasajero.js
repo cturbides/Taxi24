7018396f8b3cffed3f8eae4444d54ca2f23b0fef
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2023-02-23 15:53:47 +0000

    feat: Exception handler added to Pasajero controller & messages for different errors code added

--- src/controllers/Pasajero.js
+++ src/controllers/Pasajero.js
@@ -5,38 +5,51 @@ const Persona = require('../models/Persona');
  * Crea un nuevo pasajero y devuelve el ID
  * @param {Object} req - Objeto de solicitud HTTP
  * @param {Object} res - Objeto de respuesta HTTP
+ * @param {Function} next - Funcion next de Express
  */
-async function crear(req, res) {
+async function crear(req, res, next) {
   const {nombre, email, telefono} = req.body;
 
-  const idPersona = await Persona.crear({nombre, email, telefono});
-  const idPasajero = await Pasajero.crear(idPersona[0].id);
+  try {
+    const idPersona = await Persona.crear({nombre, email, telefono});
+    const idPasajero = await Pasajero.crear(idPersona[0].id);
 
-  res.status(201).json({id: idPasajero[0].id});
-};
+    res.status(201).json({id: idPasajero[0].id});
+  } catch (err) {
+    next(err);
+  }
+}
 
 /**
  * Devuelve una lista con todos los pasajeros existentes
  * @param {Object} _req - Objeto de solicitud HTTP
  * @param {Object} res - Objeto de respuesta HTTP
+ * @param {Function} next - Funcion next de Express
  */
-async function obtenerPasajeros(_req, res) {
-  const pasajeros = await Pasajero.obtenerTodos();
-
-  res.json(pasajeros);
+async function obtenerPasajeros(_req, res, next) {
+  try {
+    const pasajeros = await Pasajero.obtenerTodos();
+    res.json(pasajeros);
+  } catch (err) {
+    next(err);
+  }
 }
 
 /**
  * Devuelve un pasajero por su ID
  * @param {Object} req - Objeto de solicitud HTTP
  * @param {Object} res - Objeto de respuesta HTTP
+ * @param {Function} next - Funcion next de Express
  */
-async function obtenerPasajeroPorID(req, res) {
+async function obtenerPasajeroPorID(req, res, next) {
   const {id} = req.params;
 
-  const pasajero = await Pasajero.obtenerPorID(id);
-
-  res.json(pasajero);
+  try {
+    const pasajero = await Pasajero.obtenerPorID(id);
+    res.json(pasajero);
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = {
--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -6,10 +6,32 @@
  * @param {Function} _next - Function next de Express
  */
 function errorHandler(err, _req, res, _next) {
-  console.error('Error: ', err.message);
+  console.error('Error status: ', err.status);
+  console.error('Error code: ', err.code);
+  console.error('Error message: ', err.message);
 
-  const status = err.status || 400;
-  res.status(status).json({message: err.message});
+  let status = err.status || 400;
+  let message = 'Error en la solicitud. ';
+
+  switch (err.code) {
+    case '22P02':
+      message += 'Los datos proporcionados no tienen el formato adecuado';
+      break;
+
+    case '23502':
+      message += 'Existe un valor nulo donde no se permiten valores nulos';
+      break;
+
+    case '23505':
+      message += 'Un valor único en su consulta ya existe en la base de datos';
+      status = 409;
+      break;
+
+    default:
+      break;
+  }
+
+  res.status(status).json({message});
 };
 
 module.exports = errorHandler;
